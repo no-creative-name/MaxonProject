d6ff5c3deed9a068dc53522ac2ca92cc80b9f9d2
Kai Wißler <kai.wissler@gmx.de>
2017-10-16 10:32:51 +0200

    Made some little changes

--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
     $(".col").click(function(){
+        //Reset all columns to their default width
         $(this).parents().siblings().removeClass("col-1 col-9").addClass("col-3");
+        $(this).parents().children().children(".info").removeClass("is-visible");
+        //If active column is clicked
         if($(this).hasClass("col-9")) {
+            //Reset active column
             $(this).siblings().removeClass("col-9 col-1").addClass("col-3");
             $(this).toggleClass("col-3 col-9");
-            $(this).children(".info-text").toggleClass("is-visible");
         } else {
+            //Make clicked column active
             $(this).siblings().removeClass("col-9 col-3").addClass("col-1");
             $(this).removeClass("col-1 col-3").toggleClass("col-9");
             $(this).parents().children().children(".info-text").removeClass("is-visible");
-            $(this).children(".info-text").addClass("is-visible");
+            $(this).children(".info").addClass("is-visible");
         }
         
     });
