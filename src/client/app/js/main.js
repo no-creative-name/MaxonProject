6ca95b6b83986da72bb57eec15798b4838313214
Kai Wißler <kai.wissler@gmx.de>
2017-11-07 10:09:39 +0100

    Added front page and expansion functionality

--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,30 +1,46 @@
 $(document).ready(function(){
 
+    $("#landing-page").click(function() {
+        $(this).animate({left: '1920px'}, 1000);
+    });
+
     $(".col").click(function(){
-        //Reset all columns to their default width
-        $(this).parents().siblings().removeClass("col-1 col-9").addClass("col-3");
-        $(this).parents().children().children(".info").removeClass("is-visible");
+        resetAllColumnsToDefaultWidth($(this));
         //If active column is clicked
-        if($(this).hasClass("col-9")) {
-            //Reset active column
-            $(this).siblings().removeClass("col-9 col-1").addClass("col-3");
-            $(this).toggleClass("col-3 col-9");
+        if($(this).hasClass("col-active")) {
+            resetActiveColumn($(this));
         } else {
             //Make clicked column active
-            $(this).siblings().removeClass("col-9 col-3").addClass("col-1");
-            $(this).removeClass("col-1 col-3").toggleClass("col-9");
+            $(this).siblings().removeClass("col-9 col-3 col-active").addClass("col-1");
+            $(this).removeClass("col-1 col-3").toggleClass("col-9 col-active");
+            //Toggle info texts
             $(this).parents().children().children(".info-text").removeClass("is-visible");
             $(this).children(".info").addClass("is-visible");
         }
         
     });
 
-    $(".col").mouseenter(function(){
-        $(this).toggleClass("is-hovered");
-    });
-
-    $(".col").mouseleave(function(){
-        $(this).toggleClass("is-hovered");
-    });
+    menuTopOnScroll();
     
 });
+
+function resetAllColumnsToDefaultWidth (col) {
+    col.parents().siblings().removeClass("col-1 col-9").addClass("col-3");
+    col.parents().children().children(".info").removeClass("is-visible");
+}
+
+function resetActiveColumn (col) {
+    col.siblings().removeClass("col-9 col-1 col-active").addClass("col-3");
+    col.toggleClass("col-3 col-9 col-active");
+}
+
+function menuTopOnScroll () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $('.thumbnail').position().top) {
+            $('.thumbnail').attr('id', 'fixed-on-top');
+        }
+        if ($(window).scrollTop() < 100 ) {    
+            $('.thumbnail').attr('id', '');;
+        }
+    });
+}
