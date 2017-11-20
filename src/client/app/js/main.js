32c9393a022d3cc08f2baa436a6fcc1e8e4a3729
Kai Wißler <kai.wissler@gmx.de>
2017-11-20 10:13:18 +0100

    Makes icons get smaller in narrower columns

--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,52 +1,65 @@
-var aniFinished = true;
+var isAnimationFinished = true;
+var isAnyColumnActive = false;
 
 $(document).ready(function(){
 
     $("#landing-page").click(function() {
-        $(this).animate({'margin-left': '1920px'}, 1000);
+        $(this).animate({'left': 1920}, 1000);
     });
 
     $(".col").click(function(){
-        console.log(aniFinished);
-        if(aniFinished) {
-            aniFinished = false;
+        if(isAnimationFinished) {
+            isAnimationFinished = false;
             resetAllColumnsToDefaultWidth($(this));
             //If active column is clicked
             if($(this).hasClass("col-active")) {
+                $(document.body).removeClass("overflow-y-visible");
+                isAnyColumnActive = false;
                 resetActiveColumn($(this));
             } else {
+                if(!isAnyColumnActive) {
+                    $(document.body).addClass("overflow-y-visible");
+                    isAnyColumnActive = true;
+                }
                 //Make clicked column active
                 $(this).siblings().removeClass("col-9 col-3 col-active").addClass("col-1");
-                $(this).removeClass("col-1 col-3").toggleClass("col-9 col-active");
+                $(this).removeClass("col-1 col-3").addClass("col-9 col-active");
                 //Toggle info texts
                 $(this).parents().children().children(".info-text").removeClass("is-visible");
                 $(this).children(".info").addClass("is-visible");
             }
-            $(this).one('webkitTransitionEnd otransitionend msTransitionEnd transitionend', function(e) {aniFinished = true});
-        }
-        
+            console.log(isAnimationFinished);
+            setTimeout(function() {
+                isAnimationFinished = true;
+            }, 1000);
+            /*$(this).on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function (e) {
+                if($(e.target).is(this)) {
+                    isAnimationFinished = true;
+                    $(this).off('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd');
+                }
+            });*/
+        } 
     });
-
     menuTopOnScroll();
     
 });
 
 function resetAllColumnsToDefaultWidth (col) {
-    col.parents().siblings().removeClass("col-1 col-9").addClass("col-3");
+    col.siblings().removeClass("col-1 col-9").addClass("col-3");
     col.parents().children().children(".info").removeClass("is-visible");
 }
 
 function resetActiveColumn (col) {
-    col.siblings().removeClass("col-9 col-1 col-active").addClass("col-3");
+    col.siblings().removeClass("col-active");
     col.toggleClass("col-3 col-9 col-active");
 }
 
 function menuTopOnScroll () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > ($('.thumbnail').position().top - 10)) {
+        if ($(window).scrollTop() > ($('.thumbnail').position().top)) {
             $('.thumbnail').attr('id', 'fixed-on-top');
         }
-        if ($(window).scrollTop() < 150 ) {    
+        if ($(window).scrollTop() < 150-10 ) {
             $('.thumbnail').attr('id', '');;
         }
     });
