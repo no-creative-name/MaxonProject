1f463d648f2c6998e06fc3c02a5dabdf17c2e0a0
Kai Wißler <kai.wissler@gmx.de>
2017-11-23 15:19:01 +0100

    Reworks JS code structure

--- src/client/app/js/drag-handler.js
+++ src/client/app/js/drag-handler.js
@@ -1,18 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var landingPage = document.getElementById("landing-page");
-    var landingPageHammer = new Hammer(landingPage);
-    landingPageHammer.on('pan', handleLandingPageDrag);
+//  COLUMN DRAG
 
-   for(let i = 0; i < 4; i++) {
-        var column = $(".col")[i];
-        var columnHammer = new Hammer(column);
-        columnHammer.on("panleft panright", handleColumnDrag);
-    };
-});
+var isScrolling = false;
+var isAnimationFinished = true;
+var isAnyColumnActive = false;
+
+function resetSiblingColumns (column) {
+    column.parent().children().removeClass("col-1 col-9").addClass("col-3");
+    column.parent().children().children(".info").removeClass("is-visible");
+}
+
+function makeActiveColumnInactive (column) {
+    $(document.body).removeClass("overflow-y-visible");
+    isAnyColumnActive = false;
+    column.removeClass("col-9 col-active");
+}
+
+function makeInactiveColumnActive (column) {
+    if(!isAnyColumnActive) {
+        $(document.body).addClass("overflow-y-visible");
+        isAnyColumnActive = true;
+    }
+    //  Scroll to the top when you open another column
+    $(".container").scrollTop(0);
+    //  Make clicked column active
+    column.siblings().removeClass("col-9 col-3 col-active").addClass("col-1");
+    column.removeClass("col-1 col-3").addClass("col-9 col-active");
+    //  Toggle info texts
+    column.siblings().children(".info").removeClass("is-visible");
+    column.children(".info").addClass("is-visible");
+}
+
+function handleColumnDrag(ev) {
+
+    var elem = $(ev.target);
+
+    //  Check if child of col is dragged, if yes, then elem = col
+    if(!elem.hasClass("col")) {
+        elem = elem.parents(".col");
+    }
+    
+    //  Prevent animation if vertical scrolling is in action
+    var angle = Math.abs(ev.angle);
+
+    if(angle > 20 && angle < 160){
+        isScrolling = true;
+    }
+    else {
+        isScrolling = false;
+    }
+    
+    //  Actual animation
+    if(isAnimationFinished && !isScrolling) {
+        isAnimationFinished = false;
+        resetSiblingColumns(elem);
+        //  If active column is clicked
+        if(elem.hasClass("col-active")) {
+            makeActiveColumnInactive(elem);
+        } 
+        //  If inactive column is clicked
+        else {
+            makeInactiveColumnActive(elem);
+        }
+        // Timeout to prevent multiple animations to disturb each other
+        setTimeout(function() {
+            isAnimationFinished = true;
+        }, 1000);
+    } 
+
+}
+
+//  LANDING PAGE DRAG
 
 var lastPosX = 0;
 var isDragging = false;
-var isOutOfViewport = false;
+var isLandingPageOutOfViewport = false;
 var widthUntilPageSlidesAway = $(window).width()/2;
 
 function handleLandingPageDrag(ev) {
@@ -43,73 +104,33 @@ function handleLandingPageDrag(ev) {
 
     // DRAG ENDED
     // this is where we simply forget we are dragging
-    if (parseInt(elem.style.left) > widthUntilPageSlidesAway) {
-        lPage.animate({'left': 2000}, 1000);
-        isOutOfViewport = true;
-    }
-    else if (-parseInt(elem.style.left) > widthUntilPageSlidesAway) {
-        lPage.animate({'left': -2000}, 1000);
-        isOutOfViewport = true;
-    }
-    if (ev.isFinal && !isOutOfViewport) {
-        isDragging = false;
-        lPage.animate({'left': 15}, 1300, "easeOutBounce");
-    } 
-
-}
-
-var isScrolling = false;
-var isAnimationFinished = true;
-var isAnyColumnActive = false;
-
-function handleColumnDrag(ev) {
-
-    var elem = $(ev.target);
-
-    //  Check if child of col is dragged, if yes, then elem = col
-    if(!elem.hasClass("col")) {
-        elem = elem.parents(".col");
-    }
-    
-    //  Prevent animation if vertical scrolling is in action
-    var angle = Math.abs(ev.angle);
-
-    if(angle > 20 && angle < 160){
-        isScrolling = true;
-    }
-    else {
-        isScrolling = false;
-    }
-    
-    //  Actual animation
-    if(isAnimationFinished && !isScrolling) {
-        isAnimationFinished = false;
-        resetAllColumnsToDefaultWidth($("#fan-row"));
-        //  If active column is clicked
-        if(elem.hasClass("col-active")) {
-            $(document.body).removeClass("overflow-y-visible");
-            isAnyColumnActive = false;
-            elem.removeClass("col-9 col-active");
-        } 
-        //  If inactive column is clicked
-        else {
-            if(!isAnyColumnActive) {
-                $(document.body).addClass("overflow-y-visible");
-                isAnyColumnActive = true;
-            }
-            //  Scroll to the top when you open another column
-            $(".container").scrollTop(0);
-            //  Make clicked column active
-            elem.siblings().removeClass("col-9 col-3 col-active").addClass("col-1");
-            elem.removeClass("col-1 col-3").addClass("col-9 col-active");
-            //  Toggle info texts
-            elem.parents().children().children(".info-text").removeClass("is-visible");
-            elem.children(".info").addClass("is-visible");
+    if (ev.isFinal && !isLandingPageOutOfViewport) {
+        if (parseInt(elem.style.left) > widthUntilPageSlidesAway) {
+            lPage.animate({'left': 2000}, 1000);
+            isLandingPageOutOfViewport = true;
         }
-        // Timeout to prevent multiple animations to disturb each other
-        setTimeout(function() {
-            isAnimationFinished = true;
-        }, 1000);
+        else if (-parseInt(elem.style.left) > widthUntilPageSlidesAway) {
+            lPage.animate({'left': -2000}, 1000);
+            isLandingPageOutOfViewport = true;
+        }
+        else {
+            isDragging = false;
+            lPage.animate({'left': 15}, 1300, "easeOutBounce");
+        }
     } 
 
 }
+
+// GENERAL DRAG LOGIC
+
+document.addEventListener('DOMContentLoaded', function () {
+    var landingPage = document.getElementById("landing-page");
+    var landingPageHammer = new Hammer(landingPage);
+    landingPageHammer.on('pan', handleLandingPageDrag);
+
+    for(let i = 0; i < 4; i++) {
+        var column = $(".col")[i];
+        var columnHammer = new Hammer(column);
+        columnHammer.on("panleft panright", handleColumnDrag);
+    };
+});
--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,42 +1,7 @@
-//var isAnimationFinished = true;
-//var isAnyColumnActive = false;
 
 $(document).ready(function(){
-
-    /*$(".col").click(function(){
-        if(isAnimationFinished) {
-            isAnimationFinished = false;
-            resetAllColumnsToDefaultWidth($(this));
-            //If active column is clicked
-            if($(this).hasClass("col-active")) {
-                $(document.body).removeClass("overflow-y-visible");
-                isAnyColumnActive = false;
-                resetActiveColumn($(this));
-            } else {
-                if(!isAnyColumnActive) {
-                    $(document.body).addClass("overflow-y-visible");
-                    isAnyColumnActive = true;
-                }
-                //Make clicked column active
-                $(this).siblings().removeClass("col-9 col-3 col-active").addClass("col-1");
-                $(this).removeClass("col-1 col-3").addClass("col-9 col-active");
-                //Toggle info texts
-                $(this).parents().children().children(".info-text").removeClass("is-visible");
-                $(this).children(".info").addClass("is-visible");
-            }
-            setTimeout(function() {
-                isAnimationFinished = true;
-            }, 1000);
-        } 
-    });*/
     menuTopOnScroll();
-    
 });
-
-function resetAllColumnsToDefaultWidth (row) {
-    row.children().removeClass("col-1 col-9").addClass("col-3");
-    row.children().children(".info").removeClass("is-visible");
-}
 
 function menuTopOnScroll () {
     $(".container").scroll(function () {
