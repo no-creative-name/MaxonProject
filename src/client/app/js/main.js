ffe379e440e2c0f50bebdf76139bdcabf41239f6
Kai Wißler <kai.wissler@gmx.de>
2018-03-10 11:46:26 +0100

    [master] Add comment to js

--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,8 +1,9 @@
-
 $(document).ready(function(){
     menuTopOnScroll();
 });
 
+//  This function fixes the icons on top of the page
+//  once they reach the top by scrollign
 function menuTopOnScroll () {
     $(".container").scroll(function () {
         if ($(".container").scrollTop() > ($('.thumbnail').position().top)) {
--- src/client/app/js/webgl.js
+++ src/client/app/js/webgl.js
@@ -2,7 +2,6 @@ function onHomeButtonClick () {
     window.location.href = "./index.html#running";    
 }
 
-
 $(document).ready(function(){
     $('#home-button').click(function(){onHomeButtonClick()});
 });
