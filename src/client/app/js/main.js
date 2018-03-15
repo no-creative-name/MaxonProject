9f1e9dff7c055b9b94b91fc74e930ed409b75e56
Kai Wißler <kai.wissler@gmx.de>
2018-03-15 11:01:34 +0100

    [manage page transitions] Add basic fade transition between pages

--- src/client/app/js/main.js
+++ src/client/app/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);      
+    });
+
     $('#home-button').click(function(){onHomeButtonClick()});
     menuTopOnScroll();
 });
+
+$(window).on("load", function(){
+    $("body").fadeIn(1000);   
+})
+
+function redirectPage() {
+    window.location = linkLocation;
+}
 
 //  This function fixes the icons on top of the page
 //  once they reach the top by scrollign
